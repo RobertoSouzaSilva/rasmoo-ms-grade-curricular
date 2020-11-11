669e3676961245274d71093a6edef06fb8ce449d
Roberto Souza <robertosouza57@yahoo.com.br>
2020-11-11 20:40:25 -0300

    criação do primeiro controller

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -1,10 +1,20 @@
 package com.rasmoo.cliente.escola.gradecurricular;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GradeCurricularApplication {
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class GradeCurricularApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradeCurricularApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCurricularApplication.class, args);
